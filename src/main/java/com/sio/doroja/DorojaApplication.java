11073249057548fe0428bcd57c415c71e3b8ed7d
nestedbrains <nestedbrains@gmail.com>
2020-05-23 00:11:13 +0600

    cleanup

--- src/main/java/com/sio/doroja/DorojaApplication.java
+++ src/main/java/com/sio/doroja/DorojaApplication.java
@@ -2,16 +2,12 @@ package com.sio.doroja;
 
 import com.sio.doroja.models.ERole;
 import com.sio.doroja.models.Role;
-import com.sio.doroja.models.User;
 import com.sio.doroja.repository.RoleRepository;
-import com.sio.doroja.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 @SpringBootApplication
 public class DorojaApplication implements CommandLineRunner {
--- src/main/java/com/sio/doroja/payload/request/LoginRequest.java
+++ src/main/java/com/sio/doroja/payload/request/LoginRequest.java
@@ -2,7 +2,6 @@ package com.sio.doroja.payload.request;
 
 
 import lombok.Data;
-
 import javax.validation.constraints.NotBlank;
 
 @Data
